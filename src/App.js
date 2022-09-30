ae3d0034925a3284c163d8f3e7ab139c8e4d3c5a
Saurav Sinha <107345317+SauravSinha08@users.noreply.github.com>
2022-09-30 23:16:34 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Higher Order Component</h1>
+      <HOC counter={Counter}/>
     </div>
   );
 }
 
+function HOC(props){
+  return <h2><props.counter/></h2>
+}
+
+
+function Counter() {
+  const[Count, setCount] = useState(0);
+  return(
+    <div>
+    <h2>{Count}</h2>
+    <button onClick={() => setCount(Count + 1)}>Click</button>
+    </div>
+  )
+}
+
 export default App;
+
+
+
